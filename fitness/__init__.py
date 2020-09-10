2c268250106e26f220446e62d9547c071bdca83f
Kevin <khall4191@gmail.com>
2020-09-10 19:33:02 -0400

    Corrected database Account page logout button visuals (html/css)

--- fitness/__init__.py
+++ fitness/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'bdae4da6880fcd039f4764a277497e24'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitee.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from fitness import routes
